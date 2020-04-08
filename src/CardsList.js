358bb50de60df1817ddfc49c4bc18522c8eefb55
HubertNastaly <h.nastaly@gmail.com>
2020-04-08 12:04:13 +0200

    Responsiveness completed, pagination started

--- src/CardsList.js
+++ src/CardsList.js
@@ -2,47 +2,74 @@ import React from 'react';
 import PokemonCard from './PokemonCard';
 import { Grid, LinearProgress } from '@material-ui/core';
 import withWidth from '@material-ui/core/withWidth';
+import Pagination from '@material-ui/lab/Pagination';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = {
+  root:{
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  pagination:{
+    marginTop: 30,
+    marginBottom: 30
+  }
+}
 
 class CardList extends React.Component{
   constructor(props){
     super(props)
+    this.classes = props.classes
     this.state = {
+      page: 1,
+      pages: null,
       width: props.width,
       pokemonsList: [],
       isLoading: true
     }
   }
   fetchPokemons(){
-    fetch('https://pokeapi.co/api/v2/pokemon/?limit=20')
+    const limit = 20
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}`)
       .then(response => response.json())
       .then(data => {
-          this.setState({pokemonsList: data.results, isLoading: false})
+          const pages = Math.ceil(data.count / limit)
+          this.setState({
+            pages: pages,
+            pokemonsList: data.results, 
+            isLoading: false
+            })
       })
   }
   componentDidMount(){
     this.fetchPokemons();
   }
   render(){
-    const { isLoading, pokemonsList }  = this.state
-    const device = this.state.width > 600 ? this.state.width > 1280 ? 3 : 2 : 1
-    console.log(device)
-    const itemWidth = 12 / device
+    const { isLoading, pokemonsList, pages, page }  = this.state
     return(
       <div>
         {isLoading ? <LinearProgress></LinearProgress> :
-          <div style={{width: '90%', marginLeft: 'auto', marginRight: 'auto'}}>
+          <div className={this.classes.root}>
             <Grid container spacing={2}>
               {
                 pokemonsList.map(pokemon => {
                   return(
-                    <Grid item xs={itemWidth} key={pokemon.url}>
+                    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={pokemon.url}>
                       <PokemonCard pokemonUrl={pokemon.url}></PokemonCard>
                     </Grid>
                   )
                 })
               }
             </Grid>
+            <Pagination className={this.classes.pagination} 
+                        count={pages} 
+                        page={page}
+                        siblingCount={0}></Pagination>
           </div>
         }
       </div>
@@ -50,4 +77,8 @@ class CardList extends React.Component{
   }
 }
 
-export default withWidth()(CardList);
+CardList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(withWidth()(CardList));
